51642609c543a4ca3b2aa5fa7c53782afec03a5b
godi-png <eraticsyndicate@gmail.com>
2025-04-10 18:29:17 -0600

    set up main in app.cli

---
+++ app/cli/main.py
@@ -0,0 +1,19 @@
+# Terminal Interface
+
+# Run this file using 'uv run -m app.cli.main'
+"""
+from app.core.extractor import
+from app.core.models import
+from app.core.scrapper import
+"""
+
+from app.core.hello import helloWorld
+
+
+def run():
+    # Main Terminal Interface logic here
+    helloWorld()
+
+
+if __name__ == "__main__":
+    run()
